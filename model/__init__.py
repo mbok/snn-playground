db3a8e5dd157c5f4c5749d410ba775edfdb9fb2e
mbok <mboook@gmail.com>
2019-04-03 23:39:03 +0200

    Generation of synapses along axons

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import random
 from numpy.random import randn
 from typing import *
 
@@ -118,6 +119,7 @@ class Neuron:
         self.outputs.append(output)
 
     def spike(self, context: SimulationContext):
+        print("Neuron spiking: " + str(self))
         for o in self.outputs:
             o.spike(context)
 
@@ -132,32 +134,111 @@ class Snn:
         target.addInput(s)
 
     @staticmethod
-    def create(neuronCount, synapseMean=1000, synapseStdDeviation=500):
+    def create(neuronCount, synapseMeanPerNeuron=1000, synapseStdDeviationPerNeuron=500):
         snn = Snn()
-        roomSize = math.ceil(math.pow(neuronCount, 1. / 3))
-        room = np.empty((roomSize, roomSize, roomSize), dtype=object)
+        spaceSize = math.ceil(math.pow(neuronCount, 1. / 3))
+        space = np.empty((spaceSize, spaceSize, spaceSize), dtype=object)
         i = 0
         x = 0
         y = 0
         z = 0
-        posDeviation = 0.25
+        posDeviation = 0.1
         while i < neuronCount:
             n = Neuron(Membrane(tracker=NeuronTracker()),
-                       (x + randn(1)[0] * posDeviation, y + randn(1)[0] * posDeviation, z + randn(1)[0] * posDeviation))
+                       (x + randn() * posDeviation, y + randn() * posDeviation, z + randn() * posDeviation))
             snn.neurons.append(n)
-            room[x, y, z] = n
+            space[x, y, z] = n
             x += 1
-            if x >= roomSize:
+            if x >= spaceSize:
                 y += 1
                 x = 0
-            if y >= roomSize:
+            if y >= spaceSize:
                 z += 1
                 y = 0
             i += 1
-        print("Room: " + np.array2string(room))
-        synapsesCount = 1000 + randn(1)[0] * 500
-        print("Synapses count: " + str(synapsesCount))
+        totalSynapses = 0
+        for x in range(spaceSize):
+            for y in range(spaceSize):
+                for z in range(spaceSize):
+                    if space[x, y, z] is not None:
+                        synapsesCount = int(synapseMeanPerNeuron + randn() * synapseStdDeviationPerNeuron)
+                        totalSynapses += snn.connectInSpace(space[x, y, z], synapsesCount, space, spaceSize, x, y, z)
+        print("Finished SNN generation")
+        print("Total neurons: " + str(neuronCount))
+        print("Total synapses: " + str(totalSynapses))
+        print("Avg synapses per neuron: " + str(totalSynapses / neuronCount))
         return snn
+
+    def connectInSpace(self, neuron, totalCount, space, spaceSize, x, y, z):
+        connected = set()
+        axonLen = math.ceil(abs(randn() * spaceSize / 3))
+        primeAxis = random.randint(0, 2)
+        primeDir = random.choice([-1, 1])
+        step1 = random.random() * random.choice([-1, 1])
+        step2 = random.random() * random.choice([-1, 1])
+        origCount = totalCount
+        if primeAxis == 0:
+            xd = primeDir
+            yd = step1
+            zd = step2
+        elif primeAxis == 1:
+            xd = step1
+            yd = primeDir
+            zd = step2
+        else:
+            xd = step1
+            yd = step2
+            zd = primeDir
+        while axonLen > 0 and totalCount > 0:
+            totalCount -= self.connectNeighboursAt(neuron, connected, int(totalCount / axonLen), space, spaceSize, (
+                self.getBoundedIndex(spaceSize, x), self.getBoundedIndex(spaceSize, y),
+                self.getBoundedIndex(spaceSize, z)))
+            x += xd
+            y += yd
+            z += zd
+            axonLen -= 1
+        return origCount - totalCount
+
+    def getBoundedIndex(self, spaceSize, value):
+        return max(0, min(spaceSize - 1, math.ceil(value)))
+
+    def connectNeighboursAt(self, source, connected, count, space, spaceSize, atPos):
+        origCount = count
+        target = space[atPos[0], atPos[1], atPos[2]]
+        if source != target and target is not None and target not in connected:
+            self.connect(source, target)
+            connected.add(target)
+            count -= 1
+        bounds = [[atPos[0], atPos[0]], [atPos[1], atPos[1]], [atPos[2], atPos[2]]]
+        nextMovingAxis = random.randint(0, 2)
+        while count > 0 and not np.array_equal(bounds,
+                                               [[0, (spaceSize - 1)], [0, (spaceSize - 1)], [0, (spaceSize - 1)]]):
+            if random.randint(0, 1) == 1 and bounds[nextMovingAxis][0] > 0:
+                bounds[nextMovingAxis][0] -= 1
+                axisMovedPos = bounds[nextMovingAxis][0]
+            elif bounds[nextMovingAxis][1] < spaceSize - 1:
+                bounds[nextMovingAxis][1] += 1
+                axisMovedPos = bounds[nextMovingAxis][1]
+            else:
+                break
+            if nextMovingAxis == 0:
+                candidates = space[axisMovedPos, bounds[1][0]:bounds[1][1] + 1, bounds[2][0]:bounds[2][1] + 1]
+            elif nextMovingAxis == 1:
+                candidates = space[bounds[0][0]:bounds[0][1] + 1, axisMovedPos, bounds[2][0]:bounds[2][1] + 1]
+            else:
+                candidates = space[bounds[0][0]:bounds[0][1] + 1, bounds[1][0]:bounds[1][1] + 1, axisMovedPos]
+            # print("Candidates: " + str(bounds)+" / "+ np.array2string(candidates))
+            for c1 in range(len(candidates)):
+                for c2 in range(len(candidates[c1])):
+                    c = candidates[c1, c2]
+                    if source != c and c is not None and c not in connected:
+                        connected.add(c)
+                        self.connect(source, c, weight=randn())
+                        count -= 1
+                    if count <= 0:
+                        return origCount - count
+            nextMovingAxis = (nextMovingAxis + 1) % 3
+        return origCount - count
 
 
 class Simulation:
--- snn-example1.py
+++ snn-example1.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt
+import random
+from mpl_toolkits import mplot3d
 from numpy import *
 from pylab import *
+
 from model import *
-import matplotlib.animation
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+
 
 def plotNeuronTracker(tracker: NeuronTracker, titleStr):
     figure()
@@ -25,7 +27,7 @@ snn.connect(snn.neurons[0], snn.neurons[3], weight=1)
 snn.connect(snn.neurons[1], snn.neurons[3], weight=1)
 snn.connect(snn.neurons[2], snn.neurons[3], weight=1)
 
-snn = Snn.create(1500)
+snn = Snn.create(270, synapseMeanPerNeuron=10, synapseStdDeviationPerNeuron=5)
 
 tmax = 1000
 dt = 0.5
@@ -33,16 +35,17 @@ T = int(ceil(tmax / dt))
 
 sim = Simulation([snn], dt)
 
+print("Start simulation")
 # loop over time
 for t in arange(T - 1):
-    if t < 700 and random(1) < 0.75:
-        snn.neurons[0].membrane.impulse(sim.context, 10)
-    if t < 700 and random(1) < 0.75:
-        snn.neurons[1].membrane.impulse(sim.context, 10)
-    if t < 700 and random(1) < 0.25:
-        snn.neurons[2].membrane.impulse(sim.context, 10)
+    if t < 700 and random.random() < 0.75:
+        sim.context.addSpike(snn.neurons[0])
+    if t < 700 and random.random() < 0.75:
+        sim.context.addSpike(snn.neurons[1])
+    if t < 700 and random.random() < 0.25:
+        sim.context.addSpike(snn.neurons[2])
     sim.nextTick()
-
+print("Completed simulation")
 
 fig = plt.figure()
 ax = mplot3d.Axes3D(fig)
@@ -50,6 +53,13 @@ vis = ax.scatter(
     [n.position[0] for n in snn.neurons],
     [n.position[1] for n in snn.neurons],
     [n.position[2] for n in snn.neurons], c='#00ee00')
+for n in snn.neurons:
+    for s in n.outputs:
+        color = "red"
+        if s.weight < 0:
+            color = "blue"
+        ax.plot([n.position[0], s.target.position[0]], [n.position[1], s.target.position[1]],
+                zs=[n.position[2], s.target.position[2]], color=color, alpha=abs(s.weight))
 
 #
 # def animate(i):
